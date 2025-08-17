8c8a990aa1226b99aee5e4693701e4e19ba2403f
dhivya0812 <rdhivya0812@gmail.com>
2025-08-17 14:28:19 +0530

    Add files via upload
    day 1

---
+++ day1/EmployeeDetails.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class EmployeeDetails {
+	
+	public static void main(String[] args) {
+		
+		         String name = "Dhivya";
+				 int age = 24;
+				 double salary = 55000.75;
+				 char gender = 'F';
+				 boolean acceptTerms = true;
+				 float rating = 4.5f;
+				 long mobileNumber = 9876543210L;
+				 long pinCode = 56001;
+				 byte luckyNumber = 8;
+				 
+				 System.out.println("Name:"+name+"\n"
+						    +"Age:"+age+"\n"
+						    +"Salary:"+salary+"\n" 
+						    +"Gender:"+gender+"\n"
+						    +"Accepted Terms:"+acceptTerms+"\n"
+						    +"Rating:"+rating+"\n"
+						    +"Mobile Nuber:"+mobileNumber+"\n"
+						    +"PinCode:"+pinCode+"\n"
+						    +"Lucky Number:"+luckyNumber+"\n");
+	}
+
+}
---
+++ day1/Firefox.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class Firefox {
+	
+	public static void main(String[] args) {
+		
+		    double browserVersion = 100.2;
+		
+		    String browserName="firefox";
+		
+			boolean isVisible=true;
+			
+			long releaseYear=1998;
+			
+		    char browserLogo='f';
+		    
+		    System.out.println("Browser Version:"+browserVersion+"\n" 
+		    +"Name of the browser:"+browserName+"\n" 
+		    +"Is the browser visible:"+isVisible+"\n"
+		    +"Release year of the browser:"+releaseYear+"\n"
+		    +"Logo of the browser:"+browserLogo+"\n");
+		
+	}
+
+}
